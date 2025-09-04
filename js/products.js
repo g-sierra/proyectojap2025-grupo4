f8f9dda1a94aeac38cd1b71c81fd27b16748962d
fsastre95 <florenciasastre95@hotmail.com>
2025-09-04 19:56:42 -0300

    arregle errores

--- js/products.js
+++ js/products.js
@@ -14,18 +14,8 @@ fetch(URL)
           <h1>${data.catName}</h1>
         </div>
         `
-<<<<<<< HEAD
         productos = data.products;
         mostrarProductos(productos);
-=======
-        // Si no hay productos mostrar un aviso
-        if (data.products.length === 0) {
-            galeria.innerHTML += `
-            <p class="lead">No se encontraron productos en esta categoría</p> 
-            `
-        }
-
->>>>>>> 90ddbdb50965ffc10cbeba4e28ab62bc6b67c446
         data.products.forEach((producto) => {
             galeria.innerHTML += `
             <div class="producto">
@@ -44,7 +34,6 @@ fetch(URL)
         galeria.innerHTML = "<p>Error al cargar los productos.</p>";
         console.error(error);
     });
-<<<<<<< HEAD
 
 function mostrarProductos(lista) {
   galeria.innerHTML = ""; 
@@ -96,18 +85,4 @@ document.getElementById("aplicarFiltro").addEventListener("click", () => {
   const min = parseFloat(document.getElementById("precioMin").value) || 0;
   const max = parseFloat(document.getElementById("precioMax").value) || Infinity;
   filtrarPorRango(min, max);
-=======
-document.addEventListener('DOMContentLoaded', function () {
-  // Selecciona todos los elementos de producto
-  const productos = document.querySelectorAll('.producto[data-product-id]');
-  productos.forEach(function (producto) {
-    producto.addEventListener('click', function () {
-      const productId = this.getAttribute('data-product-id');
-      if (productId) {
-        localStorage.setItem('productID', productId);
-        window.location.href = 'product-info.html';
-      }
-    });
-  });
->>>>>>> 90ddbdb50965ffc10cbeba4e28ab62bc6b67c446
 });
